17fd5aeaa415b44a3531a7234dd232300f3771d8
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-12 16:30:14 -0700

    Update utils.py

--- sirius/utils.py
+++ sirius/utils.py
@@ -1,1 +1,9 @@
-#Utility functions
+from multiprocessing.pool import ThreadPool
+
+def multThread(fn, keys):
+  '''
+  Multithreading codes
+  :param fn [python function]: function
+  :param keys [list of int]: keys to multithread on
+  '''
+  raise NotImplementedError
