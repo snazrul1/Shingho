8c6588414f6a9620f7e7eb3041963961dc603d03
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-12 11:35:28 -0700

    Update stats.py

--- LighteningBuzz/stats.py
+++ LighteningBuzz/stats.py
@@ -1,6 +1,6 @@
 import numpy
 
-class sample_statistics(object):
+class basic_stats(object):
   '''
   Calculate sample statistics
   '''
@@ -22,7 +22,7 @@ class sample_statistics(object):
      :param keys [list of int]: list of keys 
      :returns [float]: mean value
      '''
-      pass
+      raise NotImplementedError
         
   def median(self, keys = 'ALL'):
      '''
@@ -30,7 +30,7 @@ class sample_statistics(object):
      :param keys [list of int]: list of keys 
      :returns [float]: mean value
      '''
-      pass
+      raise NotImplementedError
     
   def mode(self, keys = 'ALL'):
      '''
@@ -38,7 +38,7 @@ class sample_statistics(object):
      :param keys [list of int]: list of keys 
      :returns [float]: mean value
      '''
-      pass
+      raise NotImplementedError
     
   def std(self, keys = 'ALL'):
      '''
@@ -46,7 +46,7 @@ class sample_statistics(object):
      :param keys [list of int]: list of keys 
      :returns [float]: mean value
      '''
-      pass
+      raise NotImplementedError
     
   def mode(self, keys = 'ALL'):
      '''
@@ -54,60 +54,4 @@ class sample_statistics(object):
      :param keys [list of int]: list of keys 
      :returns [float]: mean value
      '''
-      pass
-    
-class population_statistics(object):
-    '''
-  Calculate population statistics
-  '''
-  
-  def __init__(self, rdd, sampling = None):
-    '''
-    :param rdd [Spark RDD]: Spark RDD for analytics
-    :param sampling [float]: Sampling rate between 0 and 1
-    '''
-    self.sampling = sampling
-    if sampling = None:
-      self.rdd = rdd
-    else:
-      self.rdd = rdd.sample(sampling)
-    
-  def mean(self, keys = 'ALL'):
-     '''
-     Calculates mean value of specific RDD
-     :param keys [list of int]: list of keys 
-     :returns [float]: mean value
-     '''
-      pass
-        
-  def median(self, keys = 'ALL'):
-     '''
-     Calculates median value 
-     :param keys [list of int]: list of keys 
-     :returns [float]: mean value
-     '''
-      pass
-    
-  def mode(self, keys = 'ALL'):
-     '''
-     Calculates mode value 
-     :param keys [list of int]: list of keys 
-     :returns [float]: mean value
-     '''
-      pass
-    
-  def std(self, keys = 'ALL'):
-     '''
-     Calculates standard deviation value 
-     :param keys [list of int]: list of keys 
-     :returns [float]: mean value
-     '''
-      pass
-    
-  def mode(self, keys = 'ALL'):
-     '''
-     Calculates variance value 
-     :param keys [list of int]: list of keys 
-     :returns [float]: mean value
-     '''
-      pass
+      raise NotImplementedError
