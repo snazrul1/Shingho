9072ba01ba579bd0f487b0538c39cceb4636f3a2
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-13 15:49:18 -0700

    Update utils.py

--- shingho/utils.py
+++ shingho/utils.py
@@ -1,12 +1,14 @@
 from multiprocessing.pool import ThreadPool
 
-def multiThread(fn, fields, threading = False):
+def multiThread(fn, fields, index_field = None, threading = False):
   '''
   Multithreading codes
   :param fn [python function]: Function on each thread
   :param fields [list of int]: Fields to multithread on
   :returns [dict]: Dictionary of keys with results
   '''
+  fn = fn(index_field = index_field)
+  
   if threading:
     pool = ThreadPool(processes = len(fields))
     results = pool.map(fn, fields)
