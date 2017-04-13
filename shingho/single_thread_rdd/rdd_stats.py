495610dcba147f630d9e12885a9f47d19aca05cd
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-13 15:59:12 -0700

    Update and rename stats.py to rdd_stats.py

--- shingho/single_thread_rdd/rdd_stats.py
+++ shingho/single_thread_rdd/rdd_stats.py
@@ -1,7 +1,7 @@
 import numpy
 from shingho.untils import multiThread
 
-class basic_stats(object):
+class basic_rdd_stats(object):
   '''
   Calculate sample statistics
   '''
@@ -17,7 +17,7 @@ class basic_stats(object):
     else:
       self.rdd = rdd.sample(sampling)
     
-  def mean(self, fields = 'ALL', index_field = None, threading = False):
+  def mean(self, fields = 'ALL', index_field = None):
      '''
      Calculates mean value of Specific RDD
      :param fields [list of int]: list of fields
@@ -25,36 +25,28 @@ class basic_stats(object):
      :param threading [bool]: Multithread each field on a thread
      :returns [dict]: dictionary of mean value with keys
      '''
-      def single_thread_mean(f):
-        '''Calculate mean for a single field on a single thread'''
-        if index_field == None:
-          mean_value = self.rdd.map(lambda row: (row[f], 1))\
-                        .reduce(lambda x,y : x+y)
-                        .map(lambda x,y : x/y)
-                        .top(1)
-          mean_dict = ('ALL', mean_value)
-        
-        else:
-          key_total = self.rdd.map(lambda row: (row[index_field], row[f]))\
-              .reduceByKey(lambda x,y : x+y)\
-              .collect()
-          key_count = self.rdd.map(lambda row: (row[index_field], 1))\
-              .reduceByKey(lambda x,y : x+y)\
-              .collect()    
-          #Change to dictionary!!!
-          mean_dict = {}
-          for k in key_total.keys:
-            mean_dict[k] = key_total[k] / key_count(k)
-        
-        return mean_dict
-      
-      mean_values = multiThread(fn = single_thread_mean, 
-                               fields = fields, 
-                               threading = threading)
-      return mean_values
-                                
-        
-  def median(self, fields = 'ALL', index_field = None, threading = False):
+    if index_field == None:
+      mean_value = self.rdd.map(lambda row: (row[f], 1))\
+                    .reduce(lambda x,y : x+y)
+                    .map(lambda x,y : x/y)
+                    .top(1)
+      mean_dict = ('ALL', mean_value)
+
+    else:
+      key_total = self.rdd.map(lambda row: (row[index_field], row[f]))\
+          .reduceByKey(lambda x,y : x+y)\
+          .collect()
+      key_count = self.rdd.map(lambda row: (row[index_field], 1))\
+          .reduceByKey(lambda x,y : x+y)\
+          .collect()    
+      #Change to dictionary!!!
+      mean_dict = {}
+      for k in key_total.keys:
+        mean_dict[k] = key_total[k] / key_count(k)
+
+    return mean_dict
+
+  def median(self, fields = 'ALL', index_field = None):
      '''
      Calculates median value 
      :param fields [list of int]: list of fields 
@@ -63,7 +55,7 @@ class basic_stats(object):
      '''
       raise NotImplementedError
     
-  def mode(self, fields = 'ALL', index_field = None, threading = False):
+  def mode(self, fields = 'ALL', index_field = None):
      '''
      Calculates mode value 
      :param fields [list of int]: list of fields
@@ -81,7 +73,7 @@ class basic_stats(object):
      '''
       raise NotImplementedError
     
-  def mode(self, fields = 'ALL', index_field = None, threading = False):
+  def mode(self, fields = 'ALL', index_field = None):
      '''
      Calculates variance value 
      :param fields [list of int]: list of fields
