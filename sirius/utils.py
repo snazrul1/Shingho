de3e829de013587298570492ec163344a86d4288
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-12 20:09:58 -0700

    Update utils.py

--- sirius/utils.py
+++ sirius/utils.py
@@ -3,7 +3,12 @@ from multiprocessing.pool import ThreadPool
 def multThread(fn, keys):
   '''
   Multithreading codes
-  :param fn [python function]: function
-  :param keys [list of int]: keys to multithread on
+  :param fn [python function]: Function on each thread
+  :param keys [list of int]: Keys to multithread on
+  :returns [dict]: Dictionary of keys with results
   '''
-  raise NotImplementedError
+  pool = ThreadPool(processes = len(keys))
+  results = pool.map(fn, keys)
+  pool.close()
+  pool.terminate()
+  return results
