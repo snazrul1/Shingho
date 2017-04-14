520ecc97320c54bc5ca98d04032b6a5f2856eeb7
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-13 18:25:01 -0700

    Update single_thread_stats.py

--- shingho/single_thread_stats.py
+++ shingho/single_thread_stats.py
@@ -41,10 +41,10 @@ class rdd_stats(object):
       mean_dict = {}
       for k in key_total.keys:
         mean_dict[k] = key_total[k] / key_count(k)
-
+        
     return mean_dict
 
-  def median(self, fields = 'ALL', index_field = None):
+  def median(self, field, index_field = None):
      '''
      Calculates median value 
      :param field [int]: field index 
@@ -53,7 +53,7 @@ class rdd_stats(object):
      '''
       raise NotImplementedError
     
-  def mode(self, fields = 'ALL', index_field = None):
+  def mode(self, field, index_field = None):
      '''
      Calculates mode value 
      :param fields [list of int]: list of fields
@@ -79,15 +79,32 @@ class rdd_stats(object):
         mode_dict[k] = mode_value_k
     return mode_dict
     
-  def std(self, fields = 'ALL', index_field = None, threading = False):
+  def std(self, field, index_field = None, threading = False):
      '''
      Calculates standard deviation value 
      :param field [int]: field index 
      :param threading [bool]: Multithread each key on a thread
      :returns [dict]: dictionary of values with keys
      '''
-      raise NotImplementedError
+    if index_field == None:
+      mean_of_sq = np.square(self.mean(self.rdd, threading = threading)[1])
+      sq_rdd = self.rdd.map(lambda row: np.square(row[field]))
+      sq_of_mean = np.square(self.mean(self.sq_rdd, threading = threading)[1])
+      std_value = np.sqrt(mean_of_sq - sq_of_mean)
+      std_dict = ('ALL', std_value)
       
+    else:
+      std_dict = {}
+      index_keys = self.rdd.map(lambda row: row[index_field]).distinct()
+      for k in index_keys:
+        std_rdd_k = self.rdd.filter(map row: row[index_field] == k)
+        mean_of_sq = np.square(self.mean(std_rdd_k, threading = threading)[1])
+        sq_rdd = std_rdd_k.map(lambda row: np.square(row[field]))
+        sq_of_mean = np.square(self.mean(sq_rdd, threading = threading)[1])
+        std_value_k = np.sqrt(mean_of_sq - sq_of_mean)
+        std_dict[k] = std_value_k
+    
+    return std_dict
 
 class df_stats(object):
   '''
