2f201c8e066b146d425574fa6513d2a60aa5a7fd
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-14 23:14:41 -0700

    Update utils.py

--- shingho/utils.py
+++ shingho/utils.py
@@ -7,15 +7,15 @@ def multithread(fn, fields, threading = False):
   :param fields [list of int]: Fields to multithread on
   :returns [dict]: Dictionary of keys with results
   '''
+  #Multithreading if threading is true
   if threading:
     pool = ThreadPool(processes = len(fields))
     results = pool.map(fn, fields)
     pool.close()
     pool.terminate()
-    
+  #Serialize if threading is false
   else:
     results = {}
     for f in fields:
       results[f] = fn(f)
-      
   return results
