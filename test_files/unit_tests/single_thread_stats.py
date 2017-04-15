9f25750dc6e3230ae700d03294e45cbfd272d2ac
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-15 11:15:45 -0700

    Update and rename stats_unit_tests.py to single_thread_stats.py

---
+++ test_files/unit_tests/single_thread_stats.py
@@ -0,0 +1,135 @@
+#To be changed to 'import shingho' only once setup.py is fixed
+try:
+  #This should work if setup.py ran successfully
+  import shingho
+except importError:
+  #If setup.py fails, manually add shingho to file path (may fail for certain dependencies)
+  import sys
+  sys.path.insert(0, '../../../shingho')
+  import shingho
+  
+from shingho import single_thread_stats
+from pyspark import SparkContext, SparkConf
+import numpy as np
+import unittest
+
+#Setup Spark Context
+conf = SparkConf().setAppName('single_thread_stats unit tests')
+sc = SparkContext(conf=conf)
+
+#Test data
+np_data = np.array([1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 5, 5, 6, 7])
+rdd_data = sc.parallelize(data)
+
+#Shingho objects for rdd_stats and df_stats
+rdd_stats_no_index = single_thread_stats.rdd_stats(rdd_data, index_field = 0)
+df_stats_no_index = single_thread_stats.rdd_stats(rdd_data = None)
+rdd_stats_index = single_thread_stats.rdd_stats(rdd_data, index_field = 0)
+df_stats_index = single_thread_stats.rdd_stats(rdd_data = None)
+
+#This unit test object will either give a 'pass' or 'fail' for each method 
+class Single_Thread_Stats_Unit_Test(unittest.TestCase):
+  '''
+  Unit Test Class
+  '''
+  def setUp(self):
+      pass
+
+  def rdd_mean_no_index(self):
+      '''
+      Cheack rdd_stats.mean() without index_field
+      '''
+      self.assertEqual(rdd_stats_no_index.mean(0)[0], np.mean(np_data))
+
+  def rdd_median_no_index(self):
+      '''
+      Cheack rdd_stats.median() without index_field
+      '''
+      self.assertEqual(rdd_stats_no_index.median(0)[0], np.median(np_data))
+
+  def rdd_mode_no_index(self):
+      '''
+      Cheack rdd_stats.mode() without index_field
+      '''
+      self.assertEqual(rdd_stats_no_index.mode(0)[0], np.mode(np_data))
+
+  def rdd_std_no_index(self):
+      '''
+      Cheack rdd_stats.std() without index_field
+      '''
+      self.assertEqual(rdd_stats_no_index.std(0)[0], np.std(np_data))
+
+  def rdd_mean_index(self):
+      '''
+      Cheack rdd_stats.mean() with index_field
+      '''
+      self.assertEqual(rdd_stats_index.mean(0)[0], np.mean(np_data))
+
+  def rdd_median_index(self):
+      '''
+      Cheack rdd_stats.median() with index_field
+      '''
+      self.assertEqual(rdd_stats_index.median(0)[0], np.median(np_data))
+
+  def rdd_mode_index(self):
+      '''
+      Cheack rdd_stats.mode() with index_field
+      '''
+      self.assertEqual(rdd_stats_index.mode(0)[0], np.mode(np_data))
+
+  def rdd_std_index(self):
+      '''
+      Cheack rdd_stats.std() with index_field
+      '''
+      self.assertEqual(rdd_stats_index.std(0)[0], np.std(np_data))
+      
+  def df_mean_no_index(self):
+      '''
+      Cheack df_stats.mean() without index_field
+      '''
+      self.assertEqual(df_stats_no_index.mean(0)[0], np.mean(np_data))
+
+  def df_median_no_index(self):
+      '''
+      Cheack df_stats.median() without index_field
+      '''
+      self.assertEqual(df_stats_no_index.median(0)[0], np.median(np_data))
+
+  def df_mode_no_index(self):
+      '''
+      Cheack df_stats.mode() without index_field
+      '''
+      self.assertEqual(df_stats_no_index.mode(0)[0], np.mode(np_data))
+
+  def df_std_no_index(self):
+      '''
+      Cheack df_stats.std() without index_field
+      '''
+      self.assertEqual(df_stats_no_index.std(0)[0], np.std(np_data))
+
+  def df_mean_index(self):
+      '''
+      Cheack df_stats.mean() with index_field
+      '''
+      self.assertEqual(df_stats_index.mean(0)[0], np.mean(np_data))
+
+  def df_median_index(self):
+      '''
+      Cheack df_stats.median() with index_field
+      '''
+      self.assertEqual(df_stats_index.median(0)[0], np.median(np_data))
+
+  def df_mode_index(self):
+      '''
+      Cheack df_stats.mode() with index_field
+      '''
+      self.assertEqual(rdd_stats_index.mode(0)[0], np.mode(np_data))
+
+  def df_std_index(self):
+      '''
+      Cheack df_stats.std() with index_field
+      '''
+      self.assertEqual(df_stats_index.std(0)[0], np.std(np_data))
+      
+if __name__ == '__main__':
+  unittest.main()
