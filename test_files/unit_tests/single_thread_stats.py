8b6d595742bc771686901adfee57082c35f3613b
Syed (Sadat) Nazrul <snazrul1@users.noreply.github.com>
2017-04-15 11:25:41 -0700

    Update single_thread_stats.py

--- test_files/unit_tests/single_thread_stats.py
+++ test_files/unit_tests/single_thread_stats.py
@@ -19,6 +19,8 @@ sc = SparkContext(conf=conf)
 
 #Test data
 np_data = np.array([1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 5, 5, 6, 7])
+pd_data = pd.DataFrame({'key_col': np_data})
+pd_data_grouped = pd_data.groupby('key_col')['key_col']
 rdd_data = sc.parallelize(data)
 
 #Shingho objects for rdd_stats and df_stats
@@ -39,97 +41,97 @@ class Single_Thread_Stats_Unit_Test(unittest.TestCase):
       '''
       Cheack rdd_stats.mean() without index_field
       '''
-      self.assertEqual(rdd_stats_no_index.mean(0)[0], np.mean(np_data))
+      self.assertEqual(rdd_stats_no_index.mean(0)['ALL'], np.mean(np_data))
 
   def rdd_median_no_index(self):
       '''
       Cheack rdd_stats.median() without index_field
       '''
-      self.assertEqual(rdd_stats_no_index.median(0)[0], np.median(np_data))
+      self.assertEqual(rdd_stats_no_index.median(0)['ALL'], np.median(np_data))
 
   def rdd_mode_no_index(self):
       '''
       Cheack rdd_stats.mode() without index_field
       '''
-      self.assertEqual(rdd_stats_no_index.mode(0)[0], np.mode(np_data))
+      self.assertEqual(rdd_stats_no_index.mode(0)['ALL'], np.mode(np_data))
 
   def rdd_std_no_index(self):
       '''
       Cheack rdd_stats.std() without index_field
       '''
-      self.assertEqual(rdd_stats_no_index.std(0)[0], np.std(np_data))
+      self.assertEqual(rdd_stats_no_index.std(0)['ALL'], np.std(np_data))
 
   def rdd_mean_index(self):
       '''
       Cheack rdd_stats.mean() with index_field
       '''
-      self.assertEqual(rdd_stats_index.mean(0)[0], np.mean(np_data))
+      self.assertEqual(rdd_stats_index.mean(0), pd_data_grouped.mean().to_dict())
 
   def rdd_median_index(self):
       '''
       Cheack rdd_stats.median() with index_field
       '''
-      self.assertEqual(rdd_stats_index.median(0)[0], np.median(np_data))
+      self.assertEqual(rdd_stats_index.median(0), pd_data_grouped.median().to_dict())
 
   def rdd_mode_index(self):
       '''
       Cheack rdd_stats.mode() with index_field
       '''
-      self.assertEqual(rdd_stats_index.mode(0)[0], np.mode(np_data))
+      self.assertEqual(rdd_stats_index.mode(0), pd_data_grouped.mode().to_dict())
 
   def rdd_std_index(self):
       '''
       Cheack rdd_stats.std() with index_field
       '''
-      self.assertEqual(rdd_stats_index.std(0)[0], np.std(np_data))
+      self.assertEqual(rdd_stats_index.std(0), pd_data_grouped.std().to_dict())
       
   def df_mean_no_index(self):
       '''
       Cheack df_stats.mean() without index_field
       '''
-      self.assertEqual(df_stats_no_index.mean(0)[0], np.mean(np_data))
+      self.assertEqual(df_stats_no_index.mean(0)['ALL'], np.mean(np_data))
 
   def df_median_no_index(self):
       '''
       Cheack df_stats.median() without index_field
       '''
-      self.assertEqual(df_stats_no_index.median(0)[0], np.median(np_data))
+      self.assertEqual(df_stats_no_index.median(0)['ALL'], np.median(np_data))
 
   def df_mode_no_index(self):
       '''
       Cheack df_stats.mode() without index_field
       '''
-      self.assertEqual(df_stats_no_index.mode(0)[0], np.mode(np_data))
+      self.assertEqual(df_stats_no_index.mode(0)['ALL'], np.mode(np_data))
 
   def df_std_no_index(self):
       '''
       Cheack df_stats.std() without index_field
       '''
-      self.assertEqual(df_stats_no_index.std(0)[0], np.std(np_data))
+      self.assertEqual(df_stats_no_index.std(0)['ALL'], np.std(np_data))
 
   def df_mean_index(self):
       '''
       Cheack df_stats.mean() with index_field
       '''
-      self.assertEqual(df_stats_index.mean(0)[0], np.mean(np_data))
+      self.assertEqual(df_stats_index.mean(0), pd_data_grouped.mean().to_dict())
 
   def df_median_index(self):
       '''
       Cheack df_stats.median() with index_field
       '''
-      self.assertEqual(df_stats_index.median(0)[0], np.median(np_data))
+      self.assertEqual(df_stats_index.median(0), pd_data_grouped.median().to_dict())
 
   def df_mode_index(self):
       '''
       Cheack df_stats.mode() with index_field
       '''
-      self.assertEqual(rdd_stats_index.mode(0)[0], np.mode(np_data))
+      self.assertEqual(rdd_stats_index.mode(0), pd_data_grouped.mode().to_dict())
 
   def df_std_index(self):
       '''
       Cheack df_stats.std() with index_field
       '''
-      self.assertEqual(df_stats_index.std(0)[0], np.std(np_data))
+      self.assertEqual(df_stats_index.std(0), pd_data_grouped.std().to_dict())
       
 if __name__ == '__main__':
   unittest.main()
